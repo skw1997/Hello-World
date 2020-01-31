db0adf69ad249c0b51db5a4f19181c7557f8c299
kaiwen shen <kshen26@wisc.edu>
2020-01-31 01:04:58 -0600

    imgview

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
